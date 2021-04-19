26642f085c1dac1ed3082a3423bd8449f642620e
12LuckyDev <12luckydev@gmail.com>
2021-04-19 16:25:28 +0200

    First operations

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,21 @@
-import { example } from '../src/';
+import { arrayHandler, ARRAY_OPERATION } from '../src/';
 
-test('TESTING example func', () => {
-  expect(example('TEST!!!')).toEqual('This is TEST!!!');
+test('TESTING arrayHandler', () => {
+  const input1 = [1, 2, 3];
+  expect(arrayHandler(input1, ARRAY_OPERATION.ADD, { newValue: 4 })).toEqual([1, 2, 3, 4]);
+  expect(arrayHandler(input1, ARRAY_OPERATION.EDIT_AT, { newValue: 4, index: 1 })).toEqual([1, 4, 3]);
+  expect(arrayHandler(input1, ARRAY_OPERATION.REMOVE_AT, { index: 1 })).toEqual([1, 3]);
+
+  const input2 = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  expect(arrayHandler(input2, ARRAY_OPERATION.EDIT_PROP_AT, { index: 1, key: 'id', propValue: 4 })).toEqual([
+    expect.objectContaining({
+      id: 1,
+    }),
+    expect.objectContaining({
+      id: 4,
+    }),
+    expect.objectContaining({
+      id: 3,
+    }),
+  ]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
-export const example = (input: string): string => `This is ${input}`;
+import { add, editAt, editPropAt, removeAt } from '@12luckydev/utils';
+
+export enum ARRAY_OPERATION {
+  ADD,
+  EDIT_AT,
+  EDIT_PROP_AT,
+  REMOVE_AT,
+}
+
+type addArgs<T> = { newValue: T };
+type editAtArgs<T> = { newValue: T; index: number };
+type removeAtArgs = { index: number };
+type editPropAtArgs<T, K extends keyof T> = { index: number; key: K; propValue: T[K] };
+
+type allArgs<T, K extends keyof T> = addArgs<T> | editAtArgs<T> | removeAtArgs | editPropAtArgs<T, K>;
+
+/**
+ * Performs the given operation on the array
+ * @param array Original array
+ * @param operation Type of operation
+ * @param args Operation arguments
+ */
+function arrayHandler<T>(array: T[], operation: ARRAY_OPERATION.ADD, args: addArgs<T>): T[];
+function arrayHandler<T>(array: T[], operation: ARRAY_OPERATION.EDIT_AT, args: editAtArgs<T>): T[];
+function arrayHandler<T, K extends keyof T>(
+  array: T[],
+  operation: ARRAY_OPERATION.EDIT_PROP_AT,
+  args: editPropAtArgs<T, K>,
+): T[];
+function arrayHandler<T>(array: T[], operation: ARRAY_OPERATION.REMOVE_AT, args: removeAtArgs): T[];
+function arrayHandler<T, K extends keyof T>(array: T[], operation: ARRAY_OPERATION, args: allArgs<T, K>): T[] {
+  switch (operation) {
+    case ARRAY_OPERATION.ADD:
+      return add(array, (args as addArgs<T>).newValue);
+    case ARRAY_OPERATION.EDIT_AT:
+      return editAt(array, (args as editAtArgs<T>).newValue, (args as editAtArgs<T>).index);
+    case ARRAY_OPERATION.EDIT_PROP_AT:
+      return editPropAt(
+        array,
+        (args as editPropAtArgs<T, keyof T>).key,
+        (args as editPropAtArgs<T, keyof T>).propValue,
+        (args as editPropAtArgs<T, keyof T>).index,
+      );
+    case ARRAY_OPERATION.REMOVE_AT:
+      return removeAt(array, (args as removeAtArgs).index);
+    default:
+      return array;
+  }
+}
+
+export { arrayHandler };
